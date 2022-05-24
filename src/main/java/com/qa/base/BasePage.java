1bf6385a9443e92079a5cf8be1d0fe9333ae82d0
Gali998 <galimannage6.gm@gmail.com>
2022-05-24 21:44:15 +0530

    explicit wait added

--- src/main/java/com/qa/base/BasePage.java
+++ src/main/java/com/qa/base/BasePage.java
@@ -3,11 +3,16 @@ package com.qa.base;
 import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BasePage {
 
     private static WebDriver driver;
+    private static int timeout = 10;
 
     public void initWebDriver(){
         ChromeDriverManager.chromedriver().setup();
@@ -29,7 +34,14 @@ public class BasePage {
 
      //click an icon
     public void clickElement(By locator){
-        driver.findElement(locator).click();
+        try {
+            WebDriverWait wait = new WebDriverWait(driver,timeout);
+            WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+            element.click();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //driver.findElement(locator).click();
     }
 
     public void typeText(By locator,String text){
@@ -37,6 +49,14 @@ public class BasePage {
     }
 
     public boolean isVisible(By locator){
-        return driver.findElement(locator).isDisplayed();
+        try {
+            WebDriverWait wait = new WebDriverWait(driver,timeout);
+            return wait.until(ExpectedConditions.visibilityOf(driver.findElement(locator)))!=null;
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        //return driver.findElement(locator).isDisplayed();
     }
 }
