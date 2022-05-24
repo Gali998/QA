95d9f3f22ae408c57a24fc753a727df8df228ce6
Gali998 <galimannage6.gm@gmail.com>
2022-05-24 17:29:49 +0530

    testng reports added

--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -1,19 +1,32 @@
 package com.qa.tests;
 
 import com.qa.base.BaseTest;
+import com.qa.pages.HomePage;
+import com.qa.pages.LoginPage;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
+    LoginPage loginPage = new LoginPage();
+    HomePage homePage = new HomePage();
+
+//    @Test
+//    public void loginTest(){
+//        openPage("https://demo.applitools.com/");
+//        typeText(By.id("username"),"test");
+//        typeText(By.id("password"),"test");
+//        clickElement(By.xpath("//input[@type='checkbox']"));
+//        clickElement(By.id("log-in"));
+//        Assert.assertTrue(isVisible(By.id("time")),"Login page not loaded");
+//    }
+
     @Test
     public void loginTest(){
         openPage("https://demo.applitools.com/");
-        typeText(By.id("username"),"test");
-        typeText(By.id("password"),"test");
-        clickElement(By.xpath("//input[@type='checkbox']"));
-        clickElement(By.id("log-in"));
-        Assert.assertTrue(isVisible(By.id("time")),"Login page not loaded");
+        loginPage.enterCredentials("test","test");
+        loginPage.clickLogin();
+        Assert.assertTrue(homePage.isTimeIconVisible(),"Login page not loaded");
     }
 }
